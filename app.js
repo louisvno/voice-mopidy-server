1d73afc91168f8ed722efdd1ace590f015dad61d
louisvno <louisvn@gmail.com>
2018-12-02 20:29:07 +0100

    voice mopidy

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
 const Promise = require('bluebird')
 const express = require('express')
 const sqlite = require('sqlite')
+const cors = require ('cors')
 const app = express()
-
+app.use(cors())
 app.get('/', (req, res) => res.send('Hello World!'))
 
 const port = process.env.PORT || 3000;
 const dbPromise = sqlite.open('./database.sqlite', { Promise });
 
-app.get('/post/:id', async (req, res, next) => {
+app.get('/track-list', async (req, res, next) => {
     res.setHeader('Content-Type', 'application/json');
   try {
     const db = await dbPromise;
-    const [post] = await Promise.all([
-      db.get('SELECT * FROM department WHERE id = ?', req.params.id),
-    ]);
-    res.send(JSON.stringify({'post': post}));
+    const tracks = await db.all('SELECT * FROM tracks');
+    console.log(tracks);
+    res.send(JSON.stringify(tracks));
   } catch (err) {
     next(err);
   }
+});
+
+app.post('/add-track', async (req, res, next) => {
+  res.setHeader('Content-Type', 'application/json');
+try {
+  const db = await dbPromise;
+  const [post] = await Promise.all([
+    db.run('INSERT INTO tracks(youtubeId) VALUES(?)', req.body.id),
+  ]);
+  res.send(JSON.stringify({'post': post}));
+} catch (err) {
+  next(err);
+}
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
